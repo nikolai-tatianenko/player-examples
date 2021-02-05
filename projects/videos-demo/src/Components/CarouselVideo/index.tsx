cec5d026ae2e046968266163e6bd6422d964e33a
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2021-02-05 11:27:10 +0000

    feat: Videos demo - Carousel video

---
+++ projects/videos-demo/src/Components/CarouselVideo/index.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import ImageCarousel, { ImageType } from "./video-carousel";
+import "./styles.css";
+
+export default function App() {
+  const [useThumbnails, setUseThumbnails] = useState(true);
+  const [images, setImages] = useState<ImageType[]>();
+
+  useEffect(() => {
+    setImages([
+      {
+        id: 1,
+        url: "https://vimeo.com/347119375",
+        title: "video1",
+      },
+      { id: 2,
+        url: "https://vimeo.com/347119375",
+         title: "video2" },
+      { id: 3,
+        url: "https://vimeo.com/347119375",
+        title: "video3" },
+      { id: 4,
+        url: "https://vimeo.com/347119375",
+        title: "video4" },
+      { id: 5,
+        url: "https://vimeo.com/347119375",
+        title: "video5" },
+    ]);
+  }, []);
+  const options: any = { thumbnail: useThumbnails, controls: true };
+
+  return (
+    <div className="App">
+      <h2>Thumbnails could be removed or added.</h2>
+      <fieldset>
+        <caption>Options</caption>
+        <button onClick={() => setUseThumbnails(!useThumbnails)}>
+          Show/hide thumbnail
+        </button>
+      </fieldset>
+      <ImageCarousel images={images} options={options} />
+    </div>
+  );
+}
