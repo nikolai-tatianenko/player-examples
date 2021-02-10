914e5bbe16484fa0d5eae8b15f407786523684d3
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2021-02-10 18:27:10 +0000

    refactor: Menu

--- projects/videos-demo/src/Components/UI/Menu.tsx
+++ projects/videos-demo/src/Components/UI/Menu.tsx
@@ -1,28 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export const Menu = () => {
+/**
+ * Default menu items for the Menu component.
+ * Each item consists of a path and label.
+ */
+const menuItemsDefault = [
+  { path: '/', label: 'Home' },
+  { path: '/modal', label: 'Modal' },
+  { path: '/custom-controls', label: 'With Custom Controls' },
+  { path: '/videos', label: '2 videos' },
+  { path: '/default-demo', label: 'Default Demo' },
+  { path: '/carousel', label: 'Carousel' },
+];
+
+/**
+ * Menu component renders a navigation menu.
+ * @param {Array} menuItems - Array of menu items with path and label.
+ * @returns {JSX.Element} - Menu component JSX.
+ */
+export const Menu = ({ menuItems = menuItemsDefault }) => {
   return (
     <nav className="main-navigation">
       <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/modal">modal</Link>
-        </li>
-        <li>
-          <Link to="/custom-controls">With Custom Controls</Link>
-        </li>
-        <li>
-          <Link to="/videos">2 videos</Link>
-        </li>
-        <li>
-          <Link to="/default-demo">Default Demo</Link>
-        </li>
-        <li>
-          <Link to="/carousel">Carousel</Link>
-        </li>
+        {menuItems.map((item, index) => (
+          <li key={index}>
+            <Link to={item.path}>{item.label}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
 };
+
+export default Menu;
