56e66996d243d2a327c4a42900ce7ce4254be47b
Nikolai Tatianenko <nikolai.tatianenko@gmail.com>
2021-02-10 11:52:10 +0000

    refactor: Carousel tests

---
+++ projects/videos-demo/src/Components/CarouselVideo/VideoCarousel.test.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import ReactPlayerCarousel, { ImageType } from "./ReactPlayerCarousel";
+
+describe("ReactPlayerCarousel", () => {
+  const images: ImageType[] = [
+    { id: 1, url: "image1.jpg", title: "Image 1" },
+    { id: 2, url: "image2.jpg", title: "Image 2" },
+    { id: 3, url: "image3.jpg", title: "Image 3" },
+  ];
+
+  const options = {
+    controls: true,
+    thumbnail: true,
+  };
+
+  it("renders without errors", () => {
+    render(<ReactPlayerCarousel images={images} options={options} />);
+  });
+
+  it("displays the first image as selected by default", () => {
+    const { getByTestId } = render(
+      <ReactPlayerCarousel images={images} options={options} />
+    );
+    const selectedImage = getByTestId("selected-image");
+    expect(selectedImage.style.backgroundImage).toBe(`url(${images[0].url})`);
+  });
+
+  it("changes the selected image on left click", () => {
+    const { getByText, getByTestId } = render(
+      <ReactPlayerCarousel images={images} options={options} />
+    );
+    const selectedImage = getByTestId("selected-image");
+    const leftButton = getByText("Prev");
+
+    fireEvent.click(leftButton);
+
+    expect(selectedImage.style.backgroundImage).toBe(`url(${images[2].url})`);
+  });
+
+  it("changes the selected image on right click", () => {
+    const { getByText, getByTestId } = render(
+      <ReactPlayerCarousel images={images} options={options} />
+    );
+    const selectedImage = getByTestId("selected-image");
+    const rightButton = getByText("Next");
+
+    fireEvent.click(rightButton);
+
+    expect(selectedImage.style.backgroundImage).toBe(`url(${images[1].url})`);
+  });
+});
